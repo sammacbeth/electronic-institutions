eb14245feb776798cfdbcb381f3ff2801e1f5425
Sam Macbeth <sm1106@imperial.ac.uk>
2014-03-28 14:15:38 +0000

    T for checking current time.

---
+++ src/main/java/uk/ac/imperial/einst/T.java
@@ -0,0 +1,24 @@
+package uk.ac.imperial.einst;
+
+public class T {
+
+	int t;
+
+	public T() {
+		super();
+	}
+
+	public int getT() {
+		return t;
+	}
+
+	public void setT(int t) {
+		this.t = t;
+	}
+
+	@Override
+	public String toString() {
+		return "t=" + t + "";
+	}
+
+}
